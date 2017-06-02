82691e6ebfbdc155e254d288e9dc9d58d1a2c3d0
Galen Rhodes <galenrhodes@me.com>
2017-06-02 11:32:47 -0400

    updates

--- Developer/download-clang-src.sh
+++ Developer/download-clang-src.sh
@@ -7,10 +7,10 @@ fi
 
 echo "Downloading clang/llvm source code..."
 
-svn co http://llvm.org/svn/llvm-project/llvm/branches/release_39              llvm
-svn co http://llvm.org/svn/llvm-project/cfe/branches/release_39               llvm/tools/clang
-svn co http://llvm.org/svn/llvm-project/compiler-rt/branches/release_39       llvm/projects/compiler-rt
-svn co http://llvm.org/svn/llvm-project/clang-tools-extra/branches/release_39 llvm/tools/clang/tools/extra/clang-tools-extra
+svn co http://llvm.org/svn/llvm-project/llvm/trunk              llvm
+svn co http://llvm.org/svn/llvm-project/cfe/trunk               llvm/tools/clang
+svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk       llvm/projects/compiler-rt
+svn co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk llvm/tools/clang/tools/extra/clang-tools-extra
 
 svn upgrade llvm
 svn upgrade llvm/tools/clang
