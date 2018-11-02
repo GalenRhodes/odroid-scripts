fa763ca1aa1c7053eaa58ed98bba686d7782a971
Galen Rhodes <galenrhodes@me.com>
2018-11-02 13:15:17 -0400

    -

--- Developer/download-clang-src.sh
+++ Developer/download-clang-src.sh
@@ -7,14 +7,14 @@ fi
 
 echo "Downloading clang/llvm source code..."
 
-svn co http://llvm.org/svn/llvm-project/llvm/trunk              llvm
-svn co http://llvm.org/svn/llvm-project/cfe/trunk               llvm/tools/clang
-svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk       llvm/projects/compiler-rt
-svn co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk llvm/tools/clang/tools/extra/clang-tools-extra
+svn co http://llvm.org/svn/llvm-project/llvm/trunk              llvm                                           || exit "$?"
+svn co http://llvm.org/svn/llvm-project/cfe/trunk               llvm/tools/clang                               || exit "$?"
+svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk       llvm/projects/compiler-rt                      || exit "$?"
+svn co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk llvm/tools/clang/tools/extra/clang-tools-extra || exit "$?"
 
-svn upgrade llvm
-svn upgrade llvm/tools/clang
-svn upgrade llvm/tools/clang/tools/extra/clang-tools-extra
-svn upgrade llvm/projects/compiler-rt
+svn upgrade llvm                                           || exit "$?"
+svn upgrade llvm/tools/clang                               || exit "$?"
+svn upgrade llvm/projects/compiler-rt                      || exit "$?"
+svn upgrade llvm/tools/clang/tools/extra/clang-tools-extra || exit "$?"
 
 exit "$?"
